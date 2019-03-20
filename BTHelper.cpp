2e63c571eaa38cd762a2555c7778a5758da23984
kongwonkeun <kongwonkeun@gmail.com>
2019-03-20 17:33:51 +0900

    it's working

--- BTHelper.cpp
+++ BTHelper.cpp
@@ -8,53 +8,53 @@ using namespace std;
 
 bool BTHelper::initialize()
 {
-	WSADATA data;
-	int  startupError = WSAStartup(MAKEWORD(2, 2), &data);
-	bool initSuccess = startupError == 0;
-	if (initSuccess) {
-		initSuccess = LOBYTE(data.wVersion) == 2 && HIBYTE(data.wVersion) == 2;
-		if (!initSuccess) {
-			BTHelper::finalize();
-		}
-	}
-	return initSuccess;
+    WSADATA data;
+    int  startupError = WSAStartup(MAKEWORD(2, 2), &data);
+    bool initSuccess = startupError == 0;
+    if (initSuccess) {
+        initSuccess = LOBYTE(data.wVersion) == 2 && HIBYTE(data.wVersion) == 2;
+        if (!initSuccess) {
+            BTHelper::finalize();
+        }
+    }
+    return initSuccess;
 }
 
 void BTHelper::finalize()
 {
-	WSACleanup();
+    WSACleanup();
 }
 
 string BTHelper::getErrorMessage(int errorCode)
 {
-	LPTSTR buffer;
-	DWORD length;
-	length = FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		errorCode,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&buffer,
-		0,
-		NULL
-	);
-	if (length == 0) {
-		return "unknow";
-	}
-	char buf[20];
-	auto result = toString(buffer);
-	LocalFree(buffer);
-	_itoa_s(errorCode, buf, sizeof(buf), 10);
-	return string("(") + buf + string(")") + result;
+    LPTSTR buffer;
+    DWORD length;
+    length = FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        errorCode,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&buffer,
+        0,
+        NULL
+    );
+    if (length == 0) {
+        return "unknow";
+    }
+    char buf[20];
+    auto result = toString(buffer);
+    LocalFree(buffer);
+    _itoa_s(errorCode, buf, sizeof(buf), 10);
+    return string("(") + buf + string(")") + result;
 }
 
 string BTHelper::toString(LPTSTR str)
 {
 #ifdef UNICODE
-	wstring_convert<codecvt_utf8<wchar_t>, wchar_t> converter;
-	return converter.to_bytes(wstring(str));
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> converter;
+    return converter.to_bytes(wstring(str));
 #else
-	return string(str);
+    return string(str);
 #endif
 }
 
--- BTSerialPort.cpp
+++ BTSerialPort.cpp
@@ -17,70 +17,132 @@ using namespace std;
 
 struct BTSerialPortSocket
 {
-	SOCKET s;
-	bool initialized;
+    SOCKET s;
+    bool initialized;
 };
+
+BTSerialPort* BTSerialPort::create(string address, int channel)
+{
+    if (channel <= 0) {
+        throw BTException("channel id should be a positive int value");
+    }
+    auto obj = new BTSerialPort(address, channel);
+    if (!obj->m_socket->initialized) {
+        delete obj;
+        throw BTException("unable to initialize socket library");
+    }
+    return obj;
+}
 
 BTSerialPort::BTSerialPort(string address, int channel) : m_address(address), m_channel(channel), m_socket(new BTSerialPortSocket)
 {
-	m_socket->s = INVALID_SOCKET;
-	m_socket->initialized = BTHelper::initialize();
+    m_socket->s = INVALID_SOCKET;
+    m_socket->initialized = BTHelper::initialize();
 }
 
 BTSerialPort::~BTSerialPort()
 {
-	if (m_socket->initialized) {
-		BTHelper::finalize();
-	}
-}
-
-BTSerialPort* BTSerialPort::createSocket(string address, int channel)
-{
-	if (channel <= 0) {
-		throw BTException("channel id should be a positive int value");
-	}
-	auto obj = new BTSerialPort(address, channel);
-	if (!obj->m_socket->initialized) {
-		delete obj;
-		throw BTException("unable to initialize socket library");
-	}
-	return obj;
+    if (m_socket->initialized) {
+        BTHelper::finalize();
+    }
 }
 
 void BTSerialPort::connectSocket()
 {
-	closeSocket();
-	int status = SOCKET_ERROR;
-	m_socket->s = socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
-	if (m_socket->s != SOCKET_ERROR) {
-		SOCKADDR_BTH addr = { 0 };
-		int size = sizeof(SOCKADDR_BTH);
-		TCHAR addrBuf[40];
-		if (m_address.length() >= 40) {
-			throw BTException("address lenght is invalid");
-		}
-		for (size_t i = 0; i < m_address.length(); i++) {
-			addrBuf[i] = (TCHAR)m_address[i];
-		}
-		addrBuf[m_address.length()] = 0;
-		status = WSAStringToAddress(addrBuf, AF_BTH, nullptr, (LPSOCKADDR)&addr, &size);
-		if (status != SOCKET_ERROR) {
-			addr.port = m_channel;
-			status = connect(m_socket->s, (LPSOCKADDR)&addr, size);
-			if (status != SOCKET_ERROR) {
-				unsigned long enableNonBlocking = 1;
-				ioctlsocket(m_socket->s, FIONBIO, &enableNonBlocking);
-			}
-		}
-	}
-	if (status != 0) {
-		string msg = BTHelper::getErrorMessage(WSAGetLastError());
-		if (m_socket->s != INVALID_SOCKET) {
-			closesocket(m_socket->s);
-		}
-		throw BTException("cannot connect: " + msg);
-	}
+    closeSocket();
+    int status = SOCKET_ERROR;
+    m_socket->s = socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
+    if (m_socket->s != SOCKET_ERROR) {
+        SOCKADDR_BTH addr = { 0 };
+        int size = sizeof(SOCKADDR_BTH);
+        TCHAR addrBuf[40];
+        if (m_address.length() >= 40) {
+            throw BTException("address lenght is invalid");
+        }
+        for (size_t i = 0; i < m_address.length(); i++) {
+            addrBuf[i] = (TCHAR)m_address[i];
+        }
+        addrBuf[m_address.length()] = 0;
+        status = WSAStringToAddress(addrBuf, AF_BTH, nullptr, (LPSOCKADDR)&addr, &size);
+        if (status != SOCKET_ERROR) {
+            addr.port = m_channel;
+            status = connect(m_socket->s, (LPSOCKADDR)&addr, size);
+            if (status != SOCKET_ERROR) {
+                unsigned long enableNonBlocking = 1;
+                ioctlsocket(m_socket->s, FIONBIO, &enableNonBlocking);
+            }
+        }
+    }
+    if (status != 0) {
+        string msg = BTHelper::getErrorMessage(WSAGetLastError());
+        if (m_socket->s != INVALID_SOCKET) {
+            closesocket(m_socket->s);
+        }
+        throw BTException("cannot connect: " + msg);
+    }
 }
 
+void BTSerialPort::closeSocket()
+{
+    if (m_socket->s != INVALID_SOCKET) {
+        closesocket(m_socket->s);
+        m_socket->s = INVALID_SOCKET;
+    }
+}
+
+int  BTSerialPort::readData(char* buffer, int length)
+{
+    if (m_socket->s == INVALID_SOCKET) {
+        throw BTException("connection has been closed");
+    }
+    if (buffer == nullptr) {
+        throw BTException("buffer cannot be null");
+    }
+    if (length == 0) {
+        return 0;
+    }
+    fd_set set;
+    FD_ZERO(&set);
+    FD_SET(m_socket->s, &set);
+    int size = -1;
+    if (select(static_cast<int>(m_socket->s) + 1, &set, nullptr, nullptr, nullptr) >= 0) {
+        if (FD_ISSET(m_socket->s, &set)) {
+            size = recv(m_socket->s, buffer, length, 0);
+        }
+        else {
+            size = 0;
+        }
+    }
+    if (size < 0) {
+        throw BTException("error reading from connection");
+    }
+    return size;
+}
+
+void BTSerialPort::writeData(const char* buffer, int length)
+{
+    if (buffer == nullptr) {
+        throw BTException("buffer cannot be null");
+    }
+    if (length == 0) {
+        return;
+    }
+    if (m_socket->s == INVALID_SOCKET) {
+        throw BTException("attempting to write to a closed connection");
+    }
+    if (send(m_socket->s, buffer, length, 0) != length) {
+        throw BTException("writing attempt was unsuccessful");
+    }
+}
+
+bool BTSerialPort::isDataAvailable()
+{
+    if (m_socket->s == INVALID_SOCKET) {
+        throw BTException("connection has been closed");
+    }
+    u_long count;
+    ioctlsocket(m_socket->s, FIONREAD, &count);
+    return count > 0;
+}
 
 
